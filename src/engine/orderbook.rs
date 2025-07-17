86c0d0035cbfae343278df2cf59c50bd0cad80bb
Washi <65116802+Washiil@users.noreply.github.com>
2025-07-16 23:12:33 -0400

    added trade struct

--- src/engine/orderbook.rs
+++ src/engine/orderbook.rs
@@ -18,25 +18,26 @@ impl OrderBook {
         }
     }
 
-    pub fn add_limit_order(&mut self, order: Order) {
-        let price = match order.kind {
-            OrderType::Limit { price } => price,
-            _ => return,
-        };
-
+    pub fn add_order(&mut self, order: Order) {
         match order.side {
-            Side::Bid => {
-                self.bids
-                    .entry(Reverse(price))
-                    .or_default()
-                    .push_back(order);
-            }
-            Side::Ask => {
-                self.asks
-                    .entry(price)
-                    .or_default()
-                    .push_back(order);
-            }
+            Side::Bid => self.add_buy_order(order),
+            Side::Ask => self.add_sell_order(order),
+        }
+    }
+
+    fn add_buy_order(&mut self, order: Order) {
+        match order.kind {
+            OrderType::Market => todo!(),
+            OrderType::Limit { price } => todo!(),
+            OrderType::Stop { stop_price } => todo!(),
+        }
+    }
+
+    fn add_sell_order(&mut self, order: Order) {
+        match order.kind {
+            OrderType::Market => todo!(),
+            OrderType::Limit { price } => todo!(),
+            OrderType::Stop { stop_price } => todo!(),
         }
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,22 +1,36 @@
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
+use chrono::{DateTime, Utc};
 
 pub type Price = Decimal;
 
+#[derive(Debug, Clone)]
 pub enum Side {
     Bid,
     Ask,
 }
 
+#[derive(Debug, Clone)]
 pub enum OrderType {
     Market,
     Limit { price: Price },
     Stop { stop_price: Price },
 }
 
+#[derive(Debug, Clone)]
 pub struct Order {
     pub id: u64,
     pub side: Side,
     pub quantity: f64,
     pub kind: OrderType,
+}
+
+#[derive(Debug, Clone)]
+pub struct Trade {
+    pub maker_order_id: u64,
+    pub taker_order_id: u64,
+    pub quantity: Decimal,
+    pub price: Price,
+    pub taker_side: Side,
+    pub timestamp: DateTime<Utc>,
 }
